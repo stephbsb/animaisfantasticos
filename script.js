ccd8e5e9788145f8567310039588ec8eaa5ec7ce
stephbsb <stephany0303@gmail.com>
2020-08-20 19:35:20 -0300

    new: adiciona accordion list style ao Faq

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-const tabmenu = document.querySelectorAll('.js-tabmenu li');
+/* Tab menu de imagens e descrição */
+function initTabNav() {
+  const tabmenu = document.querySelectorAll('.js-tabmenu li');
 
-const tabcontent = document.querySelectorAll('.js-tabcontent section');
+  const tabcontent = document.querySelectorAll('.js-tabcontent section');
 
-function activeTab(index) {
-  tabcontent.forEach((section, sectionindex) =>
-    index === sectionindex
-      ? section.classList.add('ativo')
-      : section.classList.remove('ativo')
-  );
+  function activeTab(index) {
+    tabcontent.forEach((section, sectionindex) =>
+      index === sectionindex
+        ? section.classList.add('ativo')
+        : section.classList.remove('ativo')
+    );
+  }
+
+  tabmenu.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      activeTab(index);
+    });
+  });
 }
 
-tabmenu.forEach((item, index) => {
-  item.addEventListener('click', () => {
-    activeTab(index);
-  });
+initTabNav();
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion() {
+  this.classList.toggle('ativo');
+  this.nextElementSibling.classList.toggle('ativo');
+}
+
+accordionList.forEach((accordionItem) => {
+  accordionItem.addEventListener('click', activeAccordion);
 });
